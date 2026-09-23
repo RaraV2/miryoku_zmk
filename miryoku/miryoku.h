dddcbc7ba36de8e7fbe21554096989b7abaed2e7
RaraV2 <87941142+RaraV2@users.noreply.github.com>
2026-09-23 22:56:54 +0530

    Decrease tapping term from 250 to 230

--- miryoku/miryoku.h
+++ miryoku/miryoku.h
@@ -23,7 +23,7 @@
 #define U_NA &none // present but not available for use
 #define U_NU &none // available but not used
 
-#define U_TAPPING_TERM 250
+#define U_TAPPING_TERM 230
 #define U_REQUIRE_PRIOR_IDLE 180
 
 #include "miryoku_clipboard.h"
